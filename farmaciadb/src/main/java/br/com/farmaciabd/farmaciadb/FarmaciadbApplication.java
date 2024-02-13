78862547d264393fe33f5180234109f3eebfb567
Vandielson <vandielson.tenorio.13@hotmail.com>
2024-02-13 16:51:40 -0300

    Criação do projeto

---
+++ farmaciadb/src/main/java/br/com/farmaciabd/farmaciadb/FarmaciadbApplication.java
@@ -0,0 +1,13 @@
+package br.com.farmaciabd.farmaciadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciadbApplication.class, args);
+	}
+
+}
---
+++ farmaciadb/src/test/java/br/com/farmaciabd/farmaciadb/FarmaciadbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farmaciabd.farmaciadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
